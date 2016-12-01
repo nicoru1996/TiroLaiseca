9d9e3bda795d42d9421e340e520b72d46d63f32e
nicoru1996 <nicoru1996@hotmail.com>
2016-12-01 18:31:33 -0300

    TiroLaiseca 2.0.1

--- calculo.js
+++ calculo.js
@@ -1,5 +1,31 @@
+function verificarValor(idElem){
+    if( document.getElementById(idElem).value==""){
+        alert("El Campo "+ document.getElementById(idElem).name +" ,está vacío.");
+        document.getElementById(idElem).value ="";
+    }
+}
+function verificarTodo() {
+    if (document.getElementById("velocidadInicial").value == "") {
+        alert("Complete TODOS los campos");
+        return
+    }
+    if (document.getElementById("Angulo").value == "") {
+        alert("Complete TODOS los campos");
+        return;
+    }
+    if (document.getElementById("AlturaInicial").value == "") {
+        alert("Complete TODOS los campos");
+        return;
+    }
+    else {
+        dibujar()
+    }
+}
+
 function dibujar() {
-    var ang = document.getElementById("Ángulo").value;
+
+    //Conversión de unidades para la calcuadora
+    var ang = document.getElementById("Angulo").value;
     var angUn = document.getElementById("Unidadesa0").value;
    if (angUn != "rad") {
        ang= (ang * Math.PI) / 180;
@@ -9,28 +35,36 @@ function dibujar() {
     if (vel0Un != "m/s") {
         vel0 = (vel0 * 1000) / 3600;
     }
-    var Alt0 = Number( document.getElementById("Altura Inicial").value);
+    var Alt0 = Number( document.getElementById("AlturaInicial").value);
     var alt0Un = document.getElementById("Unidadesh0").value;
     if (alt0Un != "m") {
         Alt0 =Alt0 * 1000;
     }
-    //Fijarse si las unidades son correctas... y si no, hacer conversión
-
-
-    /*
-     if(angUn!="rad"){
-     var angRad = ang*Math.PI/180;
-     }
-     else{
-     angRad=ang;
-     }
-     /*onblur="verificarValor(this.id)"
-     ....
-     */
-
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     canvas.width = canvas.width; //Blanquear canvas
+
+    //Calculos
+    if (Alt0==0){
+        var tiem = (2*vel0*Math.sin(ang))/9.81;
+    }
+    else {
+        var tiem = ((vel0*Math.sin(ang)+Math.sqrt(Math.pow(vel0*Math.sin(ang),2)+2*9.81))/9.81)
+    }
+    if (Alt0==0){
+        var tiemax = (vel0*Math.sin(ang))/9.81;
+    }
+    else {
+        var tiemax = ((vel0*Math.sin(ang)+Math.sqrt(Math.pow(vel0*Math.sin(ang),2)+2*9.81))/9.81)
+    }
+    var hmax = (Number(Alt0)+((Math.pow(vel0,2)*Math.pow(Math.sin(ang),2))/(2*9.8)));
+    var dmax = vel0*Math.cos(ang)*tiemax;
+    if (Alt0==0) {
+        var dist = ((Math.pow(vel0, 2)) * Math.sin(2 * ang)) / 9.81;
+    }
+    else{
+        var dist = vel0*Math.cos(ang)*tiem;
+    }
 
     var xMax= canvas.width;
     var yMax= canvas.height;
@@ -57,27 +91,7 @@ function dibujar() {
     ctx.arc(0+margenEjes,yMax-margenEjes,5,0,2*Math.PI); //x,y,radio circulo,
     ctx.stroke();
 
-    //Calculos
-    var tiem = (2*vel0*Math.sin(ang))/9.81;
-    if (Alt0==0){
-        var tiemax = (vel0*Math.sin(ang))/9.81;
-    }
-    else {
-        var tiemax = ((vel0*Math.sin(ang)-Math.sqrt(Math.pow(vel0*Math.sin(ang),2))+2*9.81))/9.81;
-    }
-    var hmax = (Number(Alt0)+((Math.pow(vel0,2)*Math.pow(Math.sin(ang),2))/(2*9.8)));
-    var distmax = vel0*Math.cos(ang)*tiemax;
-    if (Alt0==0) {
-        var dist = ((Math.pow(vel0, 2)) * Math.sin(2 * ang)) / 9.8;
-    }
-    else{
-        var dist = vel0*Math.cos(ang)*tiem;
-    }
-
-
-
-
-    //graficador //**Todavia no funciona con Altura Inicial. solo calculo.
+    //graficador
     for(var x=10; x<dist; ){
         yPos = (x*Math.tan(ang)-((9.8*Math.pow(x,2))/(2*Math.pow(vel0,2)*Math.pow(Math.cos(ang),2))))+Number(Alt0);
         ctx.arc(x,yMax-yPos,1,0,2*Math.PI);
@@ -87,11 +101,11 @@ function dibujar() {
         if (ang==Math.PI/2){x=x/2}
     }
 
-    //***Tabla resultados hecha con inputs, no se puede mostrar los resultados ahi.
+    //Resultados
     alert("Distancia: "+dist);
     document.getElementById("DistanciaFinal").value=Math.round(dist*100)/100;
-    alert("Distancia de alura máxima: "+distmax);
-    document.getElementById("disthmax").value=Math.round(dist*100)/100;
+    alert("Distancia de alura máxima: "+dmax);
+    document.getElementById("disthmax").value=Math.round(dmax*100)/100;
     alert("Tiempo hasta el máximo: "+tiemax);
     document.getElementById("tiemhmax").value=Math.round(tiemax*100)/100;
     alert("Tiempo de vuelo: "+tiem);
@@ -101,10 +115,3 @@ function dibujar() {
     alert("Tiempo de Altura Máxima: "+tiemax);
     
 }
-
-//function verificarValor(idElem){
-    //if(document.getElementById(idElem).value = isNaN()){
-        ///alert("El valor de "+ document.getElementById(idElem).name +" es incorrecto.");
-        //document.getElementById(idElem).value ="";
-    //}
-///}
